59e299604789cf0ba99478d7aa4bf8cc3b176e2b
ylxcvvan <894151413@qq.com>
2024-05-13 08:33:10 +0800

    修改mainwidget

---
+++ mainwidget.h
@@ -0,0 +1,26 @@
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWidget;
+}
+QT_END_NAMESPACE
+
+class MainWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainWidget(QWidget *parent = nullptr);
+    ~MainWidget();
+
+private slots:
+    void on_toolButtonMain_clicked();
+
+private:
+    Ui::MainWidget *ui;
+};
+#endif // MAINWIDGET_H
