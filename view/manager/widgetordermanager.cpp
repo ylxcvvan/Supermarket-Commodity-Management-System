5a9f62264602046fbb59bc090d619f4a95ce3b73
ylxcvvan <894151413@qq.com>
2024-05-18 19:49:27 +0800

    初步完成视图层的订单管理界面

--- view/manager/widgetordermanager.cpp
+++ view/manager/widgetordermanager.cpp
@@ -9,9 +9,11 @@ WidgetOrderManager::WidgetOrderManager(QWidget *parent)
     ui->setupUi(this);
 
     //设置表的样式
-    ui->tableView->setCornerButtonEnabled(false);
-    ui->tableView->setAlternatingRowColors(true);
-    ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableViewOrder->setCornerButtonEnabled(false);
+    ui->tableViewOrder->setAlternatingRowColors(true);
+    ui->tableViewOrder->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    InitLineEditInputMode();
 }
 
 WidgetOrderManager::~WidgetOrderManager()
@@ -20,7 +22,52 @@ WidgetOrderManager::~WidgetOrderManager()
     delete ui;
 }
 
-void WidgetOrderManager::loadModel()
+void WidgetOrderManager::loadModelOrder()
 {
+    int OrderId=searchOrderId?ui->lineEditOrderId->text().toInt():-1;
+    int CashierId=searchCashierId?ui->lineEditCashierId->text().toInt():-1;
 
 }
+
+void WidgetOrderManager::InitLineEditInputMode()
+{
+    // 设置商品ID的正则表达式
+    QRegExp rxId("[0-9]{1,10}"); // 1到10位数字
+    QRegExpValidator *validatorId = new QRegExpValidator(rxId, this);
+    ui->lineEditOrderId->setValidator(validatorId);
+    ui->lineEditCashierId->setValidator(validatorId);
+    ui->lineEditUserId->setValidator(validatorId);
+}
+
+void WidgetOrderManager::on_pushButtonOrderId_clicked(bool checked)
+{
+    searchOrderId=checked;
+}
+
+void WidgetOrderManager::on_pushButtonOrderStage_clicked(bool checked)
+{
+    searchOrderStage=checked;
+}
+
+void WidgetOrderManager::on_pushButtonCashierId_clicked(bool checked)
+{
+    searchCashierId=checked;
+}
+
+void WidgetOrderManager::on_pushButtonUserId_clicked(bool checked)
+{
+    searchUserId=checked;
+}
+
+
+void WidgetOrderManager::on_pushButtonOrderTime_clicked(bool checked)
+{
+    searchOrderTime=checked;
+}
+
+
+void WidgetOrderManager::on_pushButtonTotalPrice_clicked(bool checked)
+{
+    searchTotalPrice=checked;
+}
+
--- view/manager/widgetordermanager.h
+++ view/manager/widgetordermanager.h
@@ -14,20 +14,34 @@ class WidgetOrderManager : public QWidget
 public:
     explicit WidgetOrderManager(QWidget *parent = nullptr);
     ~WidgetOrderManager();
-    void loadModel();
-    void InitLineEditInputMode();
     void InitBoolSearchState();
+    void loadModelOrder();
+private:
+    void InitLineEditInputMode();
+
+private slots:
+    void on_pushButtonOrderId_clicked(bool checked);
+
+    void on_pushButtonOrderStage_clicked(bool checked);
+
+    void on_pushButtonCashierId_clicked(bool checked);
+
+    void on_pushButtonUserId_clicked(bool checked);
+
+    void on_pushButtonOrderTime_clicked(bool checked);
+
+    void on_pushButtonTotalPrice_clicked(bool checked);
+
 private:
     Ui::WidgetOrderManager *ui;
     OrderTableService *p_OrderTableService;
 
-    bool searchOrderId;
-    bool searchOrderTime;
-    bool searchCashierId;
-    bool searchUserId;
-    bool searchTotalPrice;
-    bool searchPaidPrice;
-    bool searchOrderStage;
+    bool searchOrderId=false;
+    bool searchOrderTime=false;
+    bool searchCashierId=false;
+    bool searchUserId=false;
+    bool searchOrderStage=false;
+    bool searchTotalPrice=false;
 };
 
 #endif // WIDGETORDERMANAGER_H
