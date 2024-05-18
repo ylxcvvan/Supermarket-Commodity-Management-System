7b406d0e6ca209753dbcdbfd58c9697ae5b47444
MUTANTSTATUE <3200260487@QQ.COM>
2024-05-18 13:54:03 +0800

    完成改变按钮大小的动画

--- view/animate/animatetoolbutton.cpp
+++ view/animate/animatetoolbutton.cpp
@@ -6,15 +6,21 @@
 
 AnimateToolButton::AnimateToolButton(QWidget *parent) :
     QToolButton(parent),
-    m_fillRatio(0.0)
+    m_fillAnimation(this,"fillRatio"),
+    m_scaleAnimation(this,"buttonSize"),
+    m_fillRatio(0.0),
+    m_buttonSize(size())
 {
     setMouseTracking(true);
-    m_animation.setTargetObject(this);
-    m_animation.setPropertyName("fillRatio");
-    m_animation.setDuration(500); // 持续时间
+    m_fillAnimation.setTargetObject(this);
+    m_fillAnimation.setDuration(200); // 持续时间
+
+
+    m_scaleAnimation.setDuration(200); // 持续时间
+
 
     // 确保动画在鼠标离开时回到起点
-    connect(&m_animation, &QPropertyAnimation::finished, this, &AnimateToolButton::onAnimationFinished);
+    connect(&m_fillAnimation, &QPropertyAnimation::finished, this, &AnimateToolButton::onAnimationFinished);
 
 }
 
@@ -28,51 +34,75 @@ void AnimateToolButton::setFillRatio(qreal ratio)
     update(); // 触发重绘
 }
 
+QSize AnimateToolButton::buttonSize() const {
+    return m_buttonSize;
+}
 
+void AnimateToolButton::setButtonSize(const QSize &size)
+{
+    m_buttonSize = size;
+    setFixedSize(size);
+}
 
 void AnimateToolButton::enterEvent(QEvent *event)
 {
-    qDebug()<<"enter";
-    startAnimate(1.0);
+
+    startFillAnimate(1.0);
+    startScaleAnimate(size()*10/9);
+
     QToolButton::enterEvent(event);
 }
 
 void AnimateToolButton::leaveEvent(QEvent *event)
 {
-     qDebug()<<"leave";
-    startAnimate(0.0);
+
+    startFillAnimate(0.0);
+    startScaleAnimate(size()*9/10);
     QToolButton::leaveEvent(event);
 }
 
 void AnimateToolButton::paintEvent(QPaintEvent *event)
 {
-    QToolButton::paintEvent(event);
+    QToolButton::paintEvent(event); // 先调用父类的绘制函数
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
 
     // 创建渐变
-    QLinearGradient gradient( 0, height() * (1 - m_fillRatio),0,height());
-    gradient.setColorAt(0, QColor(0, 0, 0, 0));
-    gradient.setColorAt(1, QColor(0, 0, 255, 100)); // 蓝色不透明
-
+    QLinearGradient gradient(0, height() * (1 - m_fillRatio), 0, height());
+    gradient.setColorAt(0, QColor(0, 0, 0, 0)); // 起始颜色透明
+    gradient.setColorAt(1, QColor(0, 0, 255, 100)); // 蓝色半透明
     painter.fillRect(rect(), gradient);
 }
 
 void AnimateToolButton::onAnimationFinished()
 {
     // 动画完成后的处理
-    if (m_animation.direction() == QPropertyAnimation::Backward)
+    if (m_fillAnimation.direction() == QPropertyAnimation::Backward)
     {
         setFillRatio(0.0); // 恢复初始状态
     }
+    if(m_scaleAnimation.direction() == QPropertyAnimation::Backward)
+    {
+        setButtonSize(m_buttonSize);
+    }
 }
 
-void AnimateToolButton::startAnimate(double endvalue)
+void AnimateToolButton::startFillAnimate(qreal endvalue)
 {
-    m_animation.stop();
-    m_animation.setStartValue(m_fillRatio);
-    m_animation.setEndValue(endvalue);
-    m_animation.start();
+    m_fillAnimation.stop();
+    m_fillAnimation.setStartValue(m_fillRatio);
+    m_fillAnimation.setEndValue(endvalue);
+    m_fillAnimation.start();
+}
+
+void AnimateToolButton::startScaleAnimate(QSize endsize)
+{
+    qDebug()<<endsize;
+    // 设置大小动画
+    m_scaleAnimation.stop(); // 停止之前的动画
+    m_scaleAnimation.setStartValue(size());
+    m_scaleAnimation.setEndValue(endsize);
+    m_scaleAnimation.start();
 }
 
--- view/animate/animatetoolbutton.h
+++ view/animate/animatetoolbutton.h
@@ -13,24 +13,32 @@ class AnimateToolButton:  public QToolButton
 {
     Q_OBJECT
     Q_PROPERTY(qreal fillRatio READ fillRatio WRITE setFillRatio)
+    Q_PROPERTY(QSize buttonSize READ buttonSize WRITE setButtonSize)
 public:
     AnimateToolButton(QWidget *parent = nullptr);
 
     qreal fillRatio() const;
     void setFillRatio(qreal ratio);
 
+    QSize buttonSize() const;
+    void setButtonSize(const QSize &size);
+
 protected:
     void enterEvent(QEvent* event) override;
     void leaveEvent(QEvent* event) override;
     void paintEvent(QPaintEvent *event) override;
 
+
 private slots:
     void onAnimationFinished();
-private:
-    QPropertyAnimation m_animation;
-    qreal m_fillRatio;
 
-    void startAnimate(double endvalue);
+private:
+    QPropertyAnimation m_fillAnimation;
+    QPropertyAnimation m_scaleAnimation;
+    qreal m_fillRatio;
+    QSize m_buttonSize;
+    void startFillAnimate(qreal endvalue);
+    void startScaleAnimate(QSize endsize);
 };
 
 #endif // BUTTONANIMATEHELPER_H
